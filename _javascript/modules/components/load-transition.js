e784fd89f03805de8475df7c1f2c0f3b7028cbe5
Eternal Twilight <2975150170@qq.com>
2024-07-05 04:33:50 +0800

    Get Infomation.

--- _javascript/modules/components/load-transition.js
+++ _javascript/modules/components/load-transition.js
@@ -8,5 +8,6 @@ export function loadTransition() {
         // 页面加载完成后，给body添加transition-active类
         var css = document.styleSheets[document.styleSheets.length - 1];
         css.insertRule("* {will-change: unset;transition: background-color .3s ease-in-out,border .3s ease-in-out,box-shadow .3s ease-in-out,color .3s ease-in-out;}", css.length);
+        console.log(css);
     });
 }
