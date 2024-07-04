2bdd66042b4d789ccaff6d2968e5a80c2309319e
Eternal Twilight <2975150170@qq.com>
2024-07-05 04:54:31 +0800

    Try load transition after page loaded.
    Optimization logic.

--- _javascript/modules/components/load-transition.js
+++ _javascript/modules/components/load-transition.js
@@ -4,6 +4,10 @@
  */
 
 export function loadTransition() {
+    var style = document.getElementById("color-mode-transition")
+    if(!(style == null)){
+        document.head.removeChild(style);
+    }
     document.addEventListener('DOMContentLoaded', function () {
         // 页面加载完成后，给body添加transition-active类
         var newStyle = document.createElement('style');
@@ -15,6 +19,7 @@ export function loadTransition() {
                             color 0.3s ease-in-out;
             }
         `;
+        newStyle.id = "color-mode-transition";
         newStyle.appendChild(document.createTextNode(cssRules));
 
         // 将新样式添加到文档的head部分
