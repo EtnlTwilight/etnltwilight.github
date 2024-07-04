8228ccb929663113943f34726bff347152f96e6e
Eternal Twilight <2975150170@qq.com>
2024-07-05 04:39:38 +0800

    Try load transition after page loaded.

--- _javascript/modules/components/load-transition.js
+++ _javascript/modules/components/load-transition.js
@@ -6,8 +6,18 @@
 export function loadTransition() {
     document.addEventListener('DOMContentLoaded', function () {
         // 页面加载完成后，给body添加transition-active类
-        var css = document.styleSheets[document.styleSheets.length - 1];
-        css.insertRule("* {will-change: unset;transition: background-color .3s ease-in-out,border .3s ease-in-out,box-shadow .3s ease-in-out,color .3s ease-in-out;}", css.length);
-        console.log(css);
+        var newStyle = document.createElement('style');
+        var cssRules = `
+            * {
+                transition: background-color 0.3s ease-in-out,
+                            border 0.3s ease-in-out,
+                            box-shadow 0.3s ease-in-out,
+                            color 0.3s ease-in-out;
+            }
+        `;
+        newStyle.appendChild(document.createTextNode(cssRules));
+
+        // 将新样式添加到文档的head部分
+        document.head.appendChild(newStyle);
     });
 }
