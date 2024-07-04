d99a232b55f04e8e7624d220ea557a7061186063
Eternal Twilight <2975150170@qq.com>
2024-07-05 04:02:30 +0800

    Try load transition after page loaded.

--- _javascript/modules/components/load-transition.js
+++ _javascript/modules/components/load-transition.js
@@ -3,8 +3,10 @@
  * to avoid a strange visual when switching page.
  */
 
-export function loadTransition(){
-    document.addEventListener('DOMContentLoaded', function() {
+export function loadTransition() {
+    document.addEventListener('DOMContentLoaded', function () {
         // 页面加载完成后，给body添加transition-active类
-        document.body.classList.add('transition-active');});
+        var css = document.styleSheets[document.styleSheets.length - 1];
+        css.insertRule("* {will-change: unset;transition: background-color .3s ease-in-out,border .3s ease-in-out,box-shadow .3s ease-in-out,color .3s ease-in-out;}", css.length);
+    });
 }
