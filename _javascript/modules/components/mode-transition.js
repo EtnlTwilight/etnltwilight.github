b0b032cbf317f99eaaf327f3dbe40a449d3be6f8
Eternal Twilight <2975150170@qq.com>
2024-07-05 20:00:18 +0800

    Adjust Page Template.

--- _javascript/modules/components/mode-transition.js
+++ _javascript/modules/components/mode-transition.js
@@ -5,7 +5,7 @@
 
 export function initTransition() {
     window.addEventListener("load", function () {
-        window.setTimeout(loadTransition, 800);
+        window.setTimeout(loadTransition, 2000);
     });
 
     window.addEventListener("unload", function () {
